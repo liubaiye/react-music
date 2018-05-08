84b4817cac1415551fcb76b7157f8969b988cc2d
iliubin <704251611@qq.com>
2018-05-08 09:00:56 +0800

    幻灯片接口切换

---
+++ src/fetch/config.js
@@ -0,0 +1,18 @@
+export const commonParams = {
+  g_tk: 1928093487,
+  inCharset: 'utf-8',
+  outCharset: 'utf-8',
+  notice: 0,
+  format: 'jsonp'
+}
+
+export const options = {
+  param: 'jsonpCallback',
+  prefix: 'jp'
+}
+
+export const ERR_OK = 0
+
+
+// WEBPACK FOOTER //
+// ./src/api/config.js
